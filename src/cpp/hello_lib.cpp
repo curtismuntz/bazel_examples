c790ffdb31661ffbaaa176024f6d3eee5ca7c865
murt <me@murt.is>
2018-11-22 17:39:08 -0800

    Change to example user

--- src/cpp/hello_lib.cpp
+++ src/cpp/hello_lib.cpp
@@ -2,7 +2,7 @@
 namespace hello_lib {
 
 std::string constructString() {
-  auto env_var = std::getenv("BALENA_USER");
+  auto env_var = std::getenv("EXAMPLE_USER");
   auto user = env_var ? env_var : std::string{"World"};
   auto str = "Hello " + user + "!";
   return str;
